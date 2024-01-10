6cb21da2f4a97dccef7189ab4505a68034292606
jackpots28 <xblackjack28x@gmail.com>
2024-01-10 14:50:28 -0600

    Got it working

--- file_handler/file_handler.py
+++ file_handler/file_handler.py
@@ -5,10 +5,32 @@ def get_subdirs(file_path: Path):
     list_of_dirs = [x for x in file_path.iterdir() if x.is_dir()]
     print(list_of_dirs)
 
+
 def check_if_file(file_path: Path)->bool:
     return file_path.exists and not file_path.is_dir()
+
 
 def print_contents(file_path: Path):
     with open(file_path) as file:
         for line_num, line_content in enumerate(file):
             print(line_num, line_content)
+
+
+def file_to_dict(file_path: Path)->dict():
+    temp_dict = dict()
+    with open(file_path, "r") as file:
+        for line_num, line_content in enumerate(file):
+            temp_dict[line_num] = str(line_content).strip()
+    return temp_dict
+
+
+def create_new_file(file_path: Path):
+    open(file_path, 'a')
+
+
+def insert_into_file(file_path: Path, insert_text: str):
+    with open(file_path, "a") as file:
+        file.write(insert_text)
+
+def text_formatter(insert_text: str)->str:
+    return f"title: {insert_text};; command: ssh {insert_text}\n"
--- main.py
+++ main.py
@@ -4,15 +4,27 @@
 from file_handler import file_handler
 from pathlib import Path
 
-src_path = Path("/stuff")
-src_file = src_path/"test.txt"
-des_file = f""
+src_path = Path("/home/simsjo-a")
+src_file = src_path/"server.list"
 
-print(list(src_path.glob("**/*")))
-default_output_name = Path("~/tabs")
+des_path = Path("/home/simsjo-a/tabs")
+des_file = des_path/"batch"
+
+default_batch_size = 6
+
+# print(list(src_path.glob("**/*")))
 
 if __name__ == "__main__":
-    file_handler.get_subdirs(src_path)
-    print(file_handler.check_if_file(src_file))
+    file_dict = file_handler.file_to_dict(src_file)
     
-    file_handler.print_contents(src_file)
+    print(file_dict.values())
+    print(len(file_dict))
+
+    default_batch_size = len(file_dict)
+
+    file_dict = {x: file_handler.text_formatter(file_dict[x]) for x in file_dict}
+
+    for item in file_dict.values():
+        file_handler.insert_into_file(des_file, item)
+
+    print(file_dict)
