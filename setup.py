b608e7e91099ba7df7bc1bf05e202ac6b14ff094
jackpots28 <xblackjack28x@gmail.com>
2024-01-23 21:29:34 -0600

    * file_handling unittest creation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="list-to-tabs",
     version="{{VERSION_VAR}}",
-    # version="2.0.0",
+    #version="2.0.1",
     description="A python package for converting newline files into konsole-tabs",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- unit_tests/test_file_handling.py
+++ unit_tests/test_file_handling.py
@@ -1,12 +1,19 @@
 import unittest
+import tempfile
 from pathlib import Path
 from file_handler_dir import file_handler
 
+temp_file = tempfile.NamedTemporaryFile()
+test_string = "cmd"
+test_output_name = Path(temp_file.name).name
+test_output_dir = "/tmp"
 
 class TestArgPassing(unittest.TestCase):
-    def test_src_file_type(self):
+    def test_file_to_dict(self):
+        self.assertEqual(file_handler.file_handler_class.file_to_dict(Path(temp_file.name)), second=dict())
 
-        self.assertEqual(file_handler.file_handler_class.file_to_dict(), second=dict)
+    def test_insert_into_file(self):
+        file_handler.file_handler_class.insert_into_file(Path(test_output_dir), test_output_name, test_string)
 
 
 if __name__ == "__main__":
